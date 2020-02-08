60675a9ae0eba947625dec9c7182a8c954d6ec19
RikezuGe <rikezuge@gmail.com>
2020-02-08 23:30:19 +0200

    Initial commit.

---
+++ Assets/Scripts/PickCorretManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickCorretManager : MonoBehaviour
+{
+
+    public GameObject[] animals;
+
+    public GameObject toFindPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
